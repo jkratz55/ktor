b253af8997c21525665866412ccde6f03cdf6211
dmitrievanthony <dmitrievanthony@gmail.com>
2019-10-28 17:31:42 +0300

    Enable iOS tests.

--- ktor-client/ktor-client-ios/darwin/src/io/ktor/client/engine/ios/IosClientEngine.kt
+++ ktor-client/ktor-client-ios/darwin/src/io/ktor/client/engine/ios/IosClientEngine.kt
@@ -32,99 +32,103 @@ internal class IosClientEngine(override val config: IosClientEngineConfig) : Htt
 
     override suspend fun execute(
         data: HttpRequestData
-    ): HttpResponseData = suspendCancellableCoroutine { continuation ->
-        val callContext = Job(data.executionContext) + coroutineContext
-        val requestTime = GMTDate()
+    ): HttpResponseData {
+        val callContext = coroutineContext + Job(data.executionContext)
+        return withContext<HttpResponseData>(callContext) {
+            suspendCancellableCoroutine { continuation ->
+                val requestTime = GMTDate()
 
-        val delegate = object : NSObject(), NSURLSessionDataDelegateProtocol {
-            val chunks = Channel<ByteArray>(Channel.UNLIMITED)
+                val delegate = object : NSObject(), NSURLSessionDataDelegateProtocol {
+                    val chunks = Channel<ByteArray>(Channel.UNLIMITED)
 
-            override fun URLSession(session: NSURLSession, dataTask: NSURLSessionDataTask, didReceiveData: NSData) {
-                val content = didReceiveData.toByteArray()
-                if (!chunks.offer(content)) throw IosHttpRequestException()
-            }
-
-            override fun URLSession(session: NSURLSession, task: NSURLSessionTask, didCompleteWithError: NSError?) {
-                chunks.close()
-
-                if (didCompleteWithError != null) {
-                    continuation.resumeWithException(IosHttpRequestException(didCompleteWithError))
-                    return
-                }
-
-                val rawResponse = task.response as NSHTTPURLResponse
-
-                @Suppress("UNCHECKED_CAST")
-                val headersDict = rawResponse.allHeaderFields as Map<String, String>
-
-                val status = HttpStatusCode.fromValue(rawResponse.statusCode.toInt())
-                val headers = buildHeaders {
-                    headersDict.mapKeys { (key, value) -> append(key, value) }
-                }
-
-                val responseBody = writer(coroutineContext, autoFlush = true) {
-                    while (!chunks.isClosedForReceive) {
-                        val chunk = chunks.receive()
-                        channel.writeFully(chunk)
+                    override fun URLSession(session: NSURLSession, dataTask: NSURLSessionDataTask, didReceiveData: NSData) {
+                        val content = didReceiveData.toByteArray()
+                        if (!chunks.offer(content)) throw IosHttpRequestException()
                     }
-                }.channel
 
-                val version = HttpProtocolVersion.HTTP_1_1
+                    override fun URLSession(session: NSURLSession, task: NSURLSessionTask, didCompleteWithError: NSError?) {
+                        chunks.close()
 
-                val response = HttpResponseData(
-                    status, requestTime, headers, version,
-                    responseBody, callContext
+                        if (didCompleteWithError != null) {
+                            continuation.resumeWithException(IosHttpRequestException(didCompleteWithError))
+                            return
+                        }
+
+                        val rawResponse = task.response as NSHTTPURLResponse
+
+                        @Suppress("UNCHECKED_CAST")
+                        val headersDict = rawResponse.allHeaderFields as Map<String, String>
+
+                        val status = HttpStatusCode.fromValue(rawResponse.statusCode.toInt())
+                        val headers = buildHeaders {
+                            headersDict.mapKeys { (key, value) -> append(key, value) }
+                        }
+
+                        val responseBody = writer(coroutineContext, autoFlush = true) {
+                            while (!chunks.isClosedForReceive) {
+                                val chunk = chunks.receive()
+                                channel.writeFully(chunk)
+                            }
+                        }.channel
+
+                        val version = HttpProtocolVersion.HTTP_1_1
+
+                        val response = HttpResponseData(
+                            status, requestTime, headers, version,
+                            responseBody, callContext
+                        )
+
+                        continuation.resume(response)
+                    }
+
+                    override fun URLSession(
+                        session: NSURLSession,
+                        task: NSURLSessionTask,
+                        willPerformHTTPRedirection: NSHTTPURLResponse,
+                        newRequest: NSURLRequest,
+                        completionHandler: (NSURLRequest?) -> Unit
+                    ) {
+                        completionHandler(null)
+                    }
+                }
+
+                val configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+                configuration.setupProxy()
+                config.sessionConfig(configuration)
+
+                val session = NSURLSession.sessionWithConfiguration(
+                    configuration,
+                    delegate, delegateQueue = NSOperationQueue.mainQueue()
                 )
 
-                continuation.resume(response)
+                val url = URLBuilder().takeFrom(data.url).buildString()
+                val nativeRequest = NSMutableURLRequest.requestWithURL(NSURL(string = url))
+
+                mergeHeaders(data.headers, data.body) { key, value ->
+                    nativeRequest.setValue(value, key)
+                }
+
+                nativeRequest.setCachePolicy(NSURLRequestReloadIgnoringCacheData)
+                nativeRequest.setHTTPMethod(data.method.value)
+
+                launch(callContext) {
+                    val content = data.body
+                    val body = when (content) {
+                        is OutgoingContent.ByteArrayContent -> content.bytes().toNSData()
+                        is OutgoingContent.WriteChannelContent -> writer(dispatcher) {
+                            content.writeTo(channel)
+                        }.channel.readRemaining().readBytes().toNSData()
+                        is OutgoingContent.ReadChannelContent -> content.readFrom().readRemaining().readBytes().toNSData()
+                        is OutgoingContent.NoContent -> null
+                        else -> throw UnsupportedContentTypeException(content)
+                    }
+
+                    body?.let { nativeRequest.setHTTPBody(it) }
+
+                    config.requestConfig(nativeRequest)
+                    session.dataTaskWithRequest(nativeRequest).resume()
+                }
             }
-
-            override fun URLSession(
-                session: NSURLSession,
-                task: NSURLSessionTask,
-                willPerformHTTPRedirection: NSHTTPURLResponse,
-                newRequest: NSURLRequest,
-                completionHandler: (NSURLRequest?) -> Unit
-            ) {
-                completionHandler(null)
-            }
-        }
-
-        val configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
-        configuration.setupProxy()
-        config.sessionConfig(configuration)
-
-        val session = NSURLSession.sessionWithConfiguration(
-            configuration,
-            delegate, delegateQueue = NSOperationQueue.mainQueue()
-        )
-
-        val url = URLBuilder().takeFrom(data.url).buildString()
-        val nativeRequest = NSMutableURLRequest.requestWithURL(NSURL(string = url))
-
-        mergeHeaders(data.headers, data.body) { key, value ->
-            nativeRequest.setValue(value, key)
-        }
-
-        nativeRequest.setCachePolicy(NSURLRequestReloadIgnoringCacheData)
-        nativeRequest.setHTTPMethod(data.method.value)
-
-        launch(callContext) {
-            val content = data.body
-            val body = when (content) {
-                is OutgoingContent.ByteArrayContent -> content.bytes().toNSData()
-                is OutgoingContent.WriteChannelContent -> writer(dispatcher) {
-                    content.writeTo(channel)
-                }.channel.readRemaining().readBytes().toNSData()
-                is OutgoingContent.ReadChannelContent -> content.readFrom().readRemaining().readBytes().toNSData()
-                is OutgoingContent.NoContent -> null
-                else -> throw UnsupportedContentTypeException(content)
-            }
-
-            body?.let { nativeRequest.setHTTPBody(it) }
-
-            config.requestConfig(nativeRequest)
-            session.dataTaskWithRequest(nativeRequest).resume()
         }
     }
 
